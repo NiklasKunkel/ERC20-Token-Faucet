4bf9d48081beafd2e3ac6c1d65b22787f18602ba
NiklasKunkel <Niklas@theKunkels.com>
2017-10-14 07:06:27 -0400

    fixed compile bugs and wrote tests

--- src/SaiFaucet.sol
+++ src/SaiFaucet.sol
@@ -2,42 +2,40 @@ pragma solidity ^0.4.15;
 
 import "ds-note/note.sol";
 import "ds-auth/auth.sol";
+import "ds-token/token.sol";
 
 contract Faucet is DSAuth, DSNote {
-	ERC20 token;	//this might need to be a ds-token
-	mapping(address => bool) hasClaimed;
+	DSToken token;
+	mapping(address => bool) public hasClaimed;
 
-	function Faucet(ERC20 _token) {
+	function Faucet(DSToken _token) public {
 		token = _token;
 	}
 
-	//modifier to check if account has already pulled from faucet 
 	modifier isNewUser() {
 		if (hasClaimed[msg.sender]) {
-			throw;						//user has already drawn SAI from faucet 
+			revert();
 		}
 		_;
 	}
 
-	//function to deposit SAI into faucet
-	function deposit() public {
-		var ok = token.transferFrom(msg.sender, this);	//not sure if this is right syntax
+	function deposit(uint wad) public returns (bool) {
+		bool ok = token.transferFrom(msg.sender, this, wad);
 		if (!ok) {
 			revert();
 		}
+		return true;
 	}
 
-	//function to pull SAI from faucet
 	function drip() public note isNewUser {
 		hasClaimed[msg.sender] = true;
-		var ok = token.transfer(msg.sender, 25);			//how does 25 amount work with decimals of SAI
+		bool ok = token.transfer(msg.sender, 25);
 		if (!ok) {
 			revert();
 		}
 	}
 
-	//function to reset user permission to pull more sai 
-	function resetUserAllocation(address user) public auth {
+	function resetUserAllocation(address user) note public auth {
 		hasClaimed[user] = false;
 	}
 }
