ee5a279ac11b1b5a992e349b6adb89ab93d492a9
NiklasKunkel <Niklas@theKunkels.com>
2017-10-14 04:32:03 -0400

    initial sketch for faucet

--- src/SaiFaucet.sol
+++ src/SaiFaucet.sol
@@ -1,4 +1,37 @@
 pragma solidity ^0.4.15;
 
-contract SaiFaucet {
+import "ds-note/note.sol";
+import "ds-auth/auth.sol";
+
+contract Faucet is DSAuth, DSNote {
+	ERC20 token;	//this might need to be a ds-token
+	mapping(address => bool) hasClaimed;
+
+	function Faucet(ERC20 _token) {
+		token = _token;
+	}
+
+	//modifier to check if account has already pulled from faucet 
+	modifier isNewUser() {
+		if hasClaimed(msg.sender) {
+			throw;						//user has already drawn SAI from faucet 
+		}
+		_;
+	}
+
+	//function to deposit SAI into faucet
+	function deposit() {
+		token.transferFrom(msg.sender, this);	//not sure if this is right syntax
+	}
+
+	//function to pull SAI from faucet
+	function drip() note isNewUser returns () {
+		token.transfer(msg.sender, 25)			//how does 25 amount work with decimals of SAI
+	}
+
+	//function to reset user permission to pull more sai 
+	function resetUserAllocation(address user) auth {
+		hasClaimed[user] = false;
+		return true;
+	}
 }
